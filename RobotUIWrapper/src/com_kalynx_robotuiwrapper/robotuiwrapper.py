c39eab05403a0940afd2a51683a41dbc6faf31db
raider001 <furnell.daniel@gmail.com>
2024-11-24 21:40:16 +1030

    Wrap Display and Keyboard keywords

--- RobotUIWrapper/src/com_kalynx_robotuiwrapper/robotuiwrapper.py
+++ RobotUIWrapper/src/com_kalynx_robotuiwrapper/robotuiwrapper.py
@@ -1,5 +1,7 @@
 from robot.libraries.Remote import Remote
-
+from robot.api.deco import keyword, library
+ 
+@library(scope='GLOBAL', version='1.0.1')
 class RobotUIWrapper(): 
 
     _remote: Remote 
@@ -10,23 +12,86 @@ class RobotUIWrapper():
         except Exception as e:
             print(e) 
 
-    def set_primary_display_reference(self, referenceName: str):
+    # Display Keywords
+    @keyword
+    def set_primary_display_reference(self, referenceName: str) -> None:
         self._remote.run_keyword("setPrimaryDisplayReference", [referenceName], None) # type: ignore
 
-    def set_display_by_id(self, displayId: int):
+    @keyword
+    def set_display_by_id(self, displayId: int) -> None:
         self._remote.run_keyword("setDisplayById", [displayId], None) # type: ignore
 
-    def set_display_reference(self, origin_reference: str, relative_state: str, new_reference: str):
+    @keyword
+    def set_display_reference(self, origin_reference: str, relative_state: str, new_reference: str) -> None:
         self._remote.run_keyword("setDisplayReference", [origin_reference,relative_state,new_reference], None) # type: ignore
 
-    def set_monitored_area(self, x: int, y: int, width: int, height:int):
+    @keyword
+    def set_monitored_area(self, x: int, y: int, width: int, height:int) -> None:
         self._remote.run_keyword("setMonitoredArea", [x,y,width,height], None) # type: ignore
     
-    def reset_monitored_area(self):
+    @keyword
+    def reset_monitored_area(self) -> None:
         self._remote.run_keyword("resetMonitoredArea", [], None) # type: ignore
 
-    def set_monitored_area_for_display_Id(self, display_id: int, x: int, y: int, width: int, height: int):
+    @keyword
+    def set_monitored_area_for_display_Id(self, display_id: int, x: int, y: int, width: int, height: int) -> None:
         self._remote.run_keyword("setMonitoredAreaForDisplayId", [display_id,x,y,width,height], None) # type: ignore
 
-    def set_monitored_area_for_display(self, display_reference: str, x: int, y: int, width: int, height: int):
+    @keyword
+    def reset_monitored_area_for_id(self, display_id: int) -> None:
+        self._remote.run_keyword("resetMonitoredAreaForId", [display_id], None)
+
+    @keyword
+    def set_monitored_area_for_display(self, display_reference: str, x: int, y: int, width: int, height: int) -> None:
         self._remote.run_keyword("setMonitoredAreaForDisplay", [display_reference,x,y,width,height], None) # type: ignore
+
+    @keyword
+    def reset_monitored_area_for_display(self, display_reference: str) -> None:
+        self._remote.run_keyword("resetMonitoredAreaForDisplay", [display_reference], None)
+
+    @keyword
+    def set_monitored_display(self, display: str) -> None:
+        self._remote.run_keyword("setMonitoredDisplay", [display], None)
+
+    @keyword
+    def get_selected_display_dimensions(self) -> dict[str, int]:
+        return self._remote.run_keyword("getSelectedDisplayDimensions", [], None) # type: ignore
+
+    @keyword
+    def get_selected_display_monitored_area(self) -> dict[str, int]:
+        return self._remote.run_keyword("getSelectedDisplayMonitoredArea", [], None) # type: ignore
+
+    @keyword
+    def get_display_dimensions(self, display: str) -> dict[str, int]:
+        return self._remote.run_keyword("getDisplayDimensions", [display], None) # type: ignore
+    
+    @keyword
+    def get_display_monitored_area(self, display: str) -> dict[str, int]:
+        return self._remote.run_keyword("getDisplayMonitoredArea", [display], None) # type: ignore
+
+    # keyboard keywords
+    @keyword
+    def type(self, message: str) -> None:
+        self._remote.run_keyword("type", [message], None)
+
+    @keyword
+    def hold_key(self, key: str) -> None:
+        self._remote.run_keyword("holdKey", [key], None)
+
+    @keyword
+    def release_key(self, key: str) -> None:
+        self._remote.run_keyword("releaseKey", [key], None)
+
+    @keyword
+    def press_keys(self, key1: str, key2: str | None = None, key3: str | None = None, key4: str | None = None, key5: str | None = None) -> None:
+        vals: dict [str, str | None] = {"key2": key2, "key3": key3, "key4": key4, "key5": key5}
+        self._remote.run_keyword("pressKeys", [key1], self._filtered(vals))
+
+    # Mouse keywords
+    @keyword
+    def move_mouse_to(self, x: int | None = None, y: int | None = None, image: str | None = None, display: str | None = None, window: str | None = None) -> None:
+        vals: dict = {"x": x, "y": y, "image": image, "display": display, "window": window}
+        self._remote.run_keyword("moveMouseTo", [], self._filtered(locals()))
+
+    def _filtered(self, args: dict) -> dict:
+        return {k: v for k, v in args.items() if v is not None}
--- RobotUIWrapper/test/robotuiwrapper_test.py
+++ RobotUIWrapper/test/robotuiwrapper_test.py
@@ -1,10 +1,16 @@
 import unittest
-from com_kalynx_robotuiwrapper.robotuiwrapper import RobotUIWrapper
+from com_kalynx_robotuiwrapper.robotuiwrapper import RobotUIWrapper # type: ignore
 class TestRobotUiWrapper(unittest.TestCase):
 
+    _sut: RobotUIWrapper = RobotUIWrapper("localhost", 7442)
     def test_set_display_by_id(self):
-        wrapper: RobotUIWrapper = RobotUIWrapper("localhost", 7442)
-        wrapper.set_display_by_id(1)
+        self._sut.set_display_by_id(1)
+    
+    def test_set_primary_display_reference(self):
+        self._sut.set_primary_display_reference("Hello")
 
+    def test_move_mouse_to(self):
+        self._sut.move_mouse_to(x=100,y=100)
+ 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
